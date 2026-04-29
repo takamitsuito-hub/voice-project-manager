c575db15844cf911b076442f5ca0f1d5353cd0f1
Mikan <takamitsu.ito@mandarinmarche.com>
2026-04-29 17:59:08 +0900

    fix supabase import path

--- app/projects/[id]/edit/page.tsx
+++ app/projects/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../../../lib/supabase";
+import { supabase } from "../../../../lib/supabase";
 import { useParams, useRouter } from "next/navigation";
 
 type RecordingSession = {
